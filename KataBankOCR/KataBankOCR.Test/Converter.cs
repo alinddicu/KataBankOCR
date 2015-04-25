e005cd71d34a75fd78d65ddaad9a2fa740a3da26
alinddicu <alinddicu@yahoo.com>
2015-04-26 00:07:23 +0200

    with sources

---
+++ KataBankOCR/KataBankOCR.Test/Converter.cs
@@ -0,0 +1,79 @@
+﻿namespace KataBankOCR.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class Converter
+    {
+        private static readonly Dictionary<string, string> LinearSymbolToDigit = new Dictionary<string, string>
+        {
+            {" _ | ||_|", "0"}
+        };
+
+        public string Convert(string text)
+        {
+            var lines = text.Split(Environment.NewLine.ToCharArray());
+            var digits = lines
+                .Where(l => !l.Equals(string.Empty))
+                .Select(l => l.ToCharArray().Select(c => c.ToString(CultureInfo.InvariantCulture)))
+                .Select(l => l.Select((s, index) => new CharacterGroupBy3(index, s)))
+                .Select(l => l.GroupBy(e => e.Index / 3)).ToArray()
+                .SelectMany(e => e.Select(i => i.ToList()))
+                .Select((o, index) => new IntermediateSymbolGroup(index, o))
+                .GroupBy(o => o.Index % 9)
+                .ToDictionary(o => o.Key)
+                .Values
+                .Select(v => new LinearDigitSymbol(v))
+                .Select(ls => ls.ToDigit())
+                .ToArray();
+
+            return string.Join(string.Empty, digits);
+        }
+
+        private struct LinearDigitSymbol
+        {
+            private readonly string _symbol;
+
+            public LinearDigitSymbol(IEnumerable<IntermediateSymbolGroup> isgs) 
+                : this()
+            {
+                _symbol = string.Join(string.Empty, isgs.SelectMany(o => o.CharacterGroups.Select(o1 => o1.Character)).ToArray());
+            }
+
+            public string ToDigit()
+            {
+                return LinearSymbolToDigit[_symbol];
+            }
+        }
+
+        private struct IntermediateSymbolGroup
+        {
+            public IntermediateSymbolGroup(int index, IEnumerable<CharacterGroupBy3> characterGroups)
+                : this()
+            {
+                Index = index;
+                CharacterGroups = characterGroups;
+            }
+
+            public int Index { get; private set; }
+
+            public IEnumerable<CharacterGroupBy3> CharacterGroups { get; private set; }
+        }
+
+        private struct CharacterGroupBy3
+        {
+            public CharacterGroupBy3(int index, string character)
+                : this()
+            {
+                Index = index;
+                Character = character;
+            }
+
+            public int Index { get; private set; }
+
+            public string Character { get; private set; }
+        }
+    }
+}
---
+++ KataBankOCR/KataBankOCR.Test/UserStory1Test.cs
@@ -0,0 +1,21 @@
+﻿namespace KataBankOCR.Test
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class UserStory1Test
+    {
+        [TestMethod]
+        [DeploymentItem("UserStory1TestCases/UseCase1.txt", "Ressources")]
+        public void GivenUseCase1TxtWhenConvertThenReturn000000000()
+        {
+            const string path = "Ressources/UseCase1.txt";
+            var text  = File.ReadAllText(path);
+            var textConverter = new Converter();
+            Check.That(textConverter.Convert(text)).IsEqualTo("000000000");
+        }
+    }
+}
