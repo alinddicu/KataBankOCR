37c0d3a8e658c9019bb3632b699121994dd0c347
Alin Dicu <alinddicu@yahoo.com>
2018-12-12 22:13:36 +0100

    CheckAllDigits passed

---
+++ KataBankOCR/V2/KataBankOCR.Tests/DigitTests.cs
@@ -0,0 +1,22 @@
+﻿namespace KataBankOCR.Tests
+{
+	using System;
+	using System.IO;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using NFluent;
+
+	using Digit;
+
+	[TestClass]
+	public class DigitTests
+	{
+		[TestMethod]
+		[DeploymentItem("Digits.txt")]
+		public void CheckAllDigits()
+		{
+			var expected = File.ReadAllText("Digits.txt");
+			var checkedString = string.Join<Digit>(Environment.NewLine + Environment.NewLine, Digit.Digits);
+			Check.That(checkedString).IsEqualTo(expected);
+		}
+	}
+}
---
+++ KataBankOCR/V2/KataBankOCR/Digit/Digit.cs
@@ -0,0 +1,67 @@
+﻿namespace KataBankOCR.Digit
+{
+	using System;
+
+	public class Digit
+	{
+		private readonly Line _top;
+		private readonly Line _middle;
+		private readonly Line _bottom;
+
+		public static readonly Digit[] Digits =
+		{
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element(' '),new Element('|')),
+				new Line(new Element('|'), new Element('_'),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element(' '),new Element(' ')),
+				new Line(new Element(' '), new Element(' '),new Element('|')),
+				new Line(new Element(' '), new Element(' '),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element(' '), new Element('_'),new Element('|')),
+				new Line(new Element('|'), new Element('_'),new Element(' '))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element(' '), new Element('_'),new Element('|')),
+				new Line(new Element(' '), new Element('_'),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element(' '),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element('|')),
+				new Line(new Element(' '), new Element(' '),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element(' ')),
+				new Line(new Element(' '), new Element('_'),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element(' '), new Element(' '),new Element('|')),
+				new Line(new Element(' '), new Element(' '),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element('|')),
+				new Line(new Element('|'), new Element('_'),new Element('|'))),
+			new Digit(
+				new Line(new Element(' '), new Element('_'),new Element(' ')),
+				new Line(new Element('|'), new Element('_'),new Element('|')),
+				new Line(new Element(' '), new Element('_'),new Element('|'))),
+		};
+
+		public Digit(Line top, Line middle, Line bottom)
+		{
+			_top = top;
+			_middle = middle;
+			_bottom = bottom;
+		}
+
+		public override string ToString()
+		{
+			return _top + Environment.NewLine + _middle + Environment.NewLine + _bottom;
+		}
+	}
+}
